74d123a576032b133179d3a1f506ab9c698114a3
waelmanai <waelmanai96@gmail.com>
2022-04-25 10:14:06 +0100

    Creating Context logic ( increament and decrement also add to cart and check if product in cart and more

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai'
+import { AiOutlineShopping } from 'react-icons/ai';
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
+
+  const { showCard, setShowCard, totalQuantities } = useStateContext();
+
   return (
     <div className='navbar-container'>
       <p className='logo'>
         <Link href="/">Wael Manai Store </Link>
       </p>
 
-      <button type='button' className='cart-icon' onClick="">
+      <button type='button' className='cart-icon' onClick={() => setShowCard(true)}>
         <AiOutlineShopping />
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+
+      {showCard && <Cart />}
     </div>
   )
 }
